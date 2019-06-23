ce0a2faf623a49ec672083ce14881c87b2f38f29
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-06-23 11:28:01 +0900

    replace hooks?

--- src/App.js
+++ src/App.js
@@ -1,70 +1,64 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { Navbar, Button } from 'react-bootstrap';
 import './App.css';
 
-class App extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`);
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
-  componentDidMount() {
-    const { renewSession } = this.props.auth;
-
+const useAuth = auth => {
+  useEffect(() => {
+    const { renewSession } = auth;
     if (localStorage.getItem('isLoggedIn') === 'true') {
       renewSession();
     }
-  }
+  }, []);
+};
 
-  render() {
-    const { isAuthenticated } = this.props.auth;
+const useGoToHandler = history => {
+  return useCallback(route => () => history.replace(`/${route}`), [history]);
+};
+const App = ({ auth, history }) => {
+  useAuth(auth);
+  const { isAuthenticated } = auth;
+  const goToHandler = useGoToHandler(history);
+  const login = useCallback(() => auth.login(), [auth]);
+  const logout = useCallback(() => auth.logout(), [auth]);
+  console.log(isAuthenticated());
 
-    return (
-      <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
-            </Navbar.Brand>
+  return (
+    <div>
+      <Navbar fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Auth0 - React</a>
+          </Navbar.Brand>
+          <Button
+            bsStyle="primary"
+            className="btn-margin"
+            onClick={goToHandler('home')}
+          >
+            Home
+          </Button>
+          {!isAuthenticated() && (
             <Button
+              id="qsLoginBtn"
               bsStyle="primary"
               className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
+              onClick={login}
             >
-              Home
+              Log In
             </Button>
-            {!isAuthenticated() && (
-              <Button
-                id="qsLoginBtn"
-                bsStyle="primary"
-                className="btn-margin"
-                onClick={this.login.bind(this)}
-              >
-                Log In
-              </Button>
-            )}
-            {isAuthenticated() && (
-              <Button
-                id="qsLogoutBtn"
-                bsStyle="primary"
-                className="btn-margin"
-                onClick={this.logout.bind(this)}
-              >
-                Log Out
-              </Button>
-            )}
-          </Navbar.Header>
-        </Navbar>
-      </div>
-    );
-  }
-}
-
+          )}
+          {isAuthenticated() && (
+            <Button
+              id="qsLogoutBtn"
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={logout}
+            >
+              Log Out
+            </Button>
+          )}
+        </Navbar.Header>
+      </Navbar>
+    </div>
+  );
+};
 export default App;
